b40ca533737f67e401999fe4af3ae850247973aa
Dusan Djordjevic <dussann1997@live.com>
2022-02-12 13:45:54 +0100

    Added public folder

--- src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Usanzadunje\app\Controllers\PostController;
-use Usanzadunje\Core\App;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = new App();
-
---
+++ src/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Usanzadunje\Core\App;
+
+require '../../vendor/autoload.php';
+
+$app = new App();
+
